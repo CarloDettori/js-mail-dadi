52bed46cd73f215e1a87fda83f75e959f061d541
CarloDettori <dettoric07@gmail.com>
2024-11-04 16:27:47 +0100

    ex snak 5

--- script/script.js
+++ script/script.js
@@ -26,7 +26,8 @@ for (let i = 0; i < mailDatabase.length; i++) {
 Gioco dei dadi
 Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
 Stabilire il vincitore, in base a chi fa il punteggio più alto.
-*/
+
+
 function rng() {
     let x = Math.floor((Math.random() * 6) + 1);
     return x;
@@ -38,7 +39,6 @@ let pairflag = false;
 console.log(`il tuo numero è ${diceSrtikeMe}`)
 console.log(`il numero del comuter è ${diceSrtikePc}`)
 
-
 if (diceSrtikePc > diceSrtikeMe) {
     console.log("Ritenta, sarai piu fortunato/a")
 } else if (diceSrtikePc < diceSrtikeMe) {
@@ -47,8 +47,26 @@ if (diceSrtikePc > diceSrtikeMe) {
     console.log("Abbiamo un pareggio ")
     pairflag = true;
     console.log("Ricarica la pagina per giocare lo spareggio")
-
 }
 if (pairflag == false) {
     console.log("Ricarica la pagina per riprovare")
 }
+*/
+
+
+/*
+snak 5
+Crea un array vuoto.
+Chiedi per 6 volte all’utente di inserire un numero,
+se è dispari inseriscilo nell’array.
+*/
+
+let sixNumers = [];
+while (sixNumers.length < 6) {
+    let num = prompt("scrivi un numero qualsiasi");
+    if (num % 2 != 0) {
+        sixNumers.push(num)
+    }
+}
+
+
